5cfbceac21bb6056fd9b01c6f3d6cf3ec96f1f2d
Henrik Ernstsson <antason90@hotmail.com>
2013-02-22 11:04:41 +0100

    Minor changes

--- src/chalmers/dax021308/ecosystem/model/agent/SimplePreyAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/SimplePreyAgent.java
@@ -7,12 +7,13 @@ import java.util.List;
 import chalmers.dax021308.ecosystem.model.population.IPopulation;
 import chalmers.dax021308.ecosystem.model.util.Position;
 import chalmers.dax021308.ecosystem.model.util.Vector;
+
 /**
  * 
- * @author Henrik
- * Its purpose is simply to hunt down the SimpleAgent (or any other agent) in a simple way
+ * @author Henrik Its purpose is simply to hunt down the SimpleAgent (or any
+ *         other agent) in a simple way
  */
-public class SimplePreyAgent extends AbstractAgent{
+public class SimplePreyAgent extends AbstractAgent {
 
 	private double maxSpeed;
 	private double visionRange;
@@ -40,11 +41,88 @@ public class SimplePreyAgent extends AbstractAgent{
 		System.err.println("ERROR IN REPRODUCE METHOD, METHOD UNDEFINED");
 		return null;
 	}
-
+	
+	/**
+	 * @author Sebastian/Henrik 
+	 * @param predators
+	 * @param preys
+	 * @param dim
+	 */
 	private void setNewVelocity(List<IPopulation> predators,
 			List<IPopulation> preys, Dimension dim) {
-		
-		
-	}
 
+		Vector preyForce = new Vector(0, 0);
+		int nVisiblePreys = 0;
+		for (IPopulation pop : preys) {
+			for (IAgent a : pop.getAgents()) {
+				Position p = a.getPosition();
+				double distance = getPosition().getDistance(p);
+				if (distance <= visionRange) {
+					Vector newForce = new Vector(this.getPosition(), p);
+					preyForce.add(newForce.multiply(1 / this.getPosition().getDistance(p) + 0.0000001));
+					nVisiblePreys++;
+				}
+			}
+		}
+
+		if (nVisiblePreys == 0) { // Be unaffacted
+			preyForce.setVector(this.getVelocity());
+		}else{
+			preyForce.multiply(1 / (double) nVisiblePreys);
+			double norm = preyForce.getNorm();
+			preyForce.multiply(maxSpeed / norm);
+		}
+		
+		/*
+		 * The environment force is at the moment defined as 1/(distance to
+		 * wall)^2.
+		 */
+		Vector environmentForce = new Vector(0, 0);
+		Position xWallLeft = new Position(0, this.getPosition().getY());
+		Position xWallRight = new Position(dim.getWidth(), this.getPosition()
+				.getY());
+		Position yWallBottom = new Position(this.getPosition().getX(), 0);
+		Position yWallTop = new Position(this.getPosition().getX(),
+				dim.getHeight());
+
+		double scale = 10;
+		double xWallLeftForce = 1 / Math.pow((1 / scale)
+				* (this.getPosition().getDistance(xWallLeft) - 1), 2);
+		double xWallRightForce = -1
+				/ Math.pow(
+						(1 / scale)
+								* (this.getPosition().getDistance(xWallRight) - 1),
+						2);
+		double yWallBottomForce = 1 / Math.pow((1 / scale)
+				* (this.getPosition().getDistance(yWallBottom) - 1), 2);
+		double yWallTopForce = -1
+				/ Math.pow(
+						(1 / scale)
+								* (this.getPosition().getDistance(yWallTop) - 1),
+						2);
+
+		double xForce = (xWallLeftForce + xWallRightForce);
+		double yForce = (yWallBottomForce + yWallTopForce);
+
+		environmentForce.setVector(xForce, yForce);
+		// System.out.println("Environment: " + environmentForce.toString() +
+		// " | predatorForce: " + predatorForce.toString());
+		// Rescale the new velocity to not exceed maxSpeed
+		Vector acceleration = environmentForce.add(preyForce);
+		double accelerationNorm = acceleration.getNorm();
+		if (accelerationNorm > maxAcceleration) {
+			// Scales the norm of the vector back to maxSpeed
+			acceleration.multiply(maxAcceleration / accelerationNorm);
+		}
+
+		Vector newVelocity = this.getVelocity().add(acceleration);
+		double speed = newVelocity.getNorm();
+		if (speed > maxAcceleration) {
+			// Scales the norm of the vector back to maxSpeed
+			newVelocity.multiply(maxSpeed / speed);
+		}
+
+		this.getVelocity().setVector(newVelocity);
+
+	}
 }
