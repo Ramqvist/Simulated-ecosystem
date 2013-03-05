5e93aa2aa77725157aa590ec019ffe69debde9ca
Albin Bramstång <a.bramstang@gmail.com>
2013-03-05 16:43:26 +0100

    Corrected bugs.

--- src/chalmers/dax021308/ecosystem/model/agent/RabbitAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/RabbitAgent.java
@@ -13,22 +13,25 @@ import chalmers.dax021308.ecosystem.model.util.Vector;
 
 public class RabbitAgent extends AbstractAgent {
 
+	private Position gridPosition;
+	
 	public RabbitAgent(Position position, String name, Color color, 
 			int width, int height, Vector velocity, Gender gender,
 			double maxSpeed, double visionRange, double maxAcceleration) {
 		super(name, position, color, width, height, velocity, maxSpeed, visionRange, maxAcceleration);
 
 		super.gender = gender;
+		gridPosition = position;
 	}
 
 	@Override
 	public List<IAgent> reproduce(IAgent agent, int populationSize) {
 		List<IAgent> kids = new ArrayList<IAgent>();
-//		if (Math.random() < 0.0001) {
-//			RabbitAgent r = new RabbitAgent(new Position(position), name, color, width, height, new Vector(velocity), null, maxSpeed, visionRange, maxAcceleration);
-//			kids.add(r);
-//			EcoWorld.worldGrid.add(r);
-//		}
+		if (Math.random() < 0.0001) {
+			RabbitAgent r = new RabbitAgent(new Position(position), name, color, width, height, new Vector(velocity), null, maxSpeed, visionRange, maxAcceleration);
+			kids.add(r);
+			EcoWorld.worldGrid.add(r);
+		}
 		return kids;
 	}
 
@@ -44,8 +47,23 @@ public class RabbitAgent extends AbstractAgent {
 		Vector neutralForce = getNeutralForce2();
 		changeDirection();
 		nextPosition = position.addVector(velocity.add(getEnvironmentForce(dim)).add(neutralForce));
+		
+		if (nextPosition.getX() < 0) {
+			nextPosition.setPosition(0, nextPosition.getY());
+		} else if (nextPosition.getX() > dim.width) {
+			nextPosition.setPosition(dim.width, nextPosition.getY());
+		}
+		
+		if (nextPosition.getY() < 0) {
+			nextPosition.setPosition(nextPosition.getX(), 0);
+		} else if (nextPosition.getY() > dim.height) {
+			nextPosition.setPosition(nextPosition.getX(), dim.height);
+		}
+		
 		velocity = velocity.toUnitVector().multiply(length);
-		EcoWorld.worldGrid.updatePosition(this, oldPosition, nextPosition);
+		if (EcoWorld.worldGrid.updatePosition(this, gridPosition, nextPosition)) {
+			gridPosition = nextPosition;
+		}
 		
 		double after = System.nanoTime() - before;
 //		System.out.println("Time: "+(int)(after/1000) + " microsec");
--- src/chalmers/dax021308/ecosystem/model/environment/EcoWorld.java
+++ src/chalmers/dax021308/ecosystem/model/environment/EcoWorld.java
@@ -191,7 +191,7 @@ public class EcoWorld {
 
 	private List<IPopulation> createInitialPopulations(Dimension dim) {
 		List<IPopulation> populations = new ArrayList<IPopulation>();
-		IPopulation rabbits = new RabbitPopulation(500, dim, "Rabbits", Color.lightGray, 10, 50, 5);
+		IPopulation rabbits = new RabbitPopulation(100, dim, "Rabbits", Color.lightGray, 10, 50, 5);
 		rabbits.addNeutralPopulation(rabbits);
 		populations.add(rabbits);
 		
