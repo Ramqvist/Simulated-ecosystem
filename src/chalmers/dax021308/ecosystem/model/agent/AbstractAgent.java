0ab74518b7238b4cada359927d11e492dbcf10c4
Henrik Ernstsson <antason90@hotmail.com>
2013-03-05 09:58:52 +0100

    Changed the way grass reproduces

--- src/chalmers/dax021308/ecosystem/model/agent/AbstractAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/AbstractAgent.java
@@ -9,10 +9,10 @@ import chalmers.dax021308.ecosystem.model.population.IPopulation;
 import chalmers.dax021308.ecosystem.model.util.Gender;
 import chalmers.dax021308.ecosystem.model.util.Position;
 import chalmers.dax021308.ecosystem.model.util.Vector;
+
 /**
  * 
- * @author Henrik
- * Abstract class for handling the dummy methods
+ * @author Henrik Abstract class for handling the dummy methods
  */
 public abstract class AbstractAgent implements IAgent {
 	protected Position position;
@@ -32,9 +32,10 @@ public abstract class AbstractAgent implements IAgent {
 	protected final static double WALL_CONSTANT = 1;
 	protected static final double VELOCITY_DECAY = 1;
 	protected static final double RANDOM_FORCE_MAGNITUDE = 0;
-	
-	public AbstractAgent(String name, Position p, Color c, int width, int height, Vector velocity, 
-			double maxSpeed, double visionRange, double maxAcceleration){
+
+	public AbstractAgent(String name, Position p, Color c, int width,
+			int height, Vector velocity, double maxSpeed, double visionRange,
+			double maxAcceleration) {
 		this.name = name;
 		position = p;
 		color = c;
@@ -46,25 +47,30 @@ public abstract class AbstractAgent implements IAgent {
 		this.maxAcceleration = maxAcceleration;
 		this.capacity = Integer.MAX_VALUE;
 	}
-	
-	public AbstractAgent(String name, Position p, Color c, int width, int height, Vector velocity, 
-			double maxSpeed, double visionRange, double maxAcceleration, int capacity){
-		this(name, p, c, width, height, velocity, maxSpeed, visionRange, maxAcceleration);
+
+	public AbstractAgent(String name, Position p, Color c, int width,
+			int height, Vector velocity, double maxSpeed, double visionRange,
+			double maxAcceleration, int capacity) {
+		this(name, p, c, width, height, velocity, maxSpeed, visionRange,
+				maxAcceleration);
 		this.capacity = capacity;
 	}
-	
+
 	/**
 	 * Clone constructed. Use this to create a copy.
+	 * 
 	 * @param a
 	 */
 	public AbstractAgent(AbstractAgent a) {
-		 this(a.name, a.position, a.color, a.width, a.height, a.velocity, a.maxSpeed, a.visionRange, a.maxAcceleration);
+		this(a.name, new Position(a.position), a.color, a.width, a.height,
+				new Vector(a.velocity), a.maxSpeed, a.visionRange,
+				a.maxAcceleration);
 	}
-	
-	public AbstractAgent(String name, Position pos, Color color,
-			int width, int height) {
+
+	public AbstractAgent(String name, Position pos, Color color, int width,
+			int height) {
 		this.name = name;
-		position = pos;
+		position = new Position(pos);
 		this.color = color;
 		this.width = width;
 		this.height = height;
@@ -115,7 +121,7 @@ public abstract class AbstractAgent implements IAgent {
 	public void setFitness(double fitness) {
 		this.fitness = fitness;
 	}
-	
+
 	@Override
 	public void updatePosition() {
 		this.position = new Position(nextPosition);
@@ -125,23 +131,27 @@ public abstract class AbstractAgent implements IAgent {
 	public Gender getGender() {
 		return gender;
 	}
-	
+
 	@Override
 	public IAgent cloneAgent() throws CloneNotSupportedException {
 		return (IAgent) clone();
 	}
-	
+
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
-		AbstractAgent a = new AbstractAgent(name, new Position(position), color, width, height, new Vector(velocity), maxSpeed, visionRange, maxAcceleration) {
+		AbstractAgent a = new AbstractAgent(name, position, color, width,
+				height, velocity, maxSpeed, visionRange, maxAcceleration) {
 			@Override
 			public void calculateNextPosition(List<IPopulation> predators,
-					List<IPopulation> preys, List<IPopulation> neutral, Dimension dim) {
+					List<IPopulation> preys, List<IPopulation> neutral,
+					Dimension dim) {
 			}
+
 			@Override
 			public List<IAgent> reproduce(IAgent agent, int populationSize) {
 				return Collections.emptyList();
 			}
+
 			@Override
 			public String toBinaryString() {
 				// TODO Auto-generated method stub
@@ -151,152 +161,177 @@ public abstract class AbstractAgent implements IAgent {
 		};
 		return a;
 	}
-	
+
 	/**
 	 * @author Sebbe
-	 * @param neutral the population of neutral agents that this agent should be separated from (not collide with).
-	 * @return a vector with the force that this agent feels from other neutral agents in order not to collide with them.
-	 * <p>
-	 * Warning! Not optimal for linked-lists, due to O(n) complexity of linked list get(n) method. 
-	 * TODO: Special method for linked list using collection.iterator(), hasNext() & next().
+	 * @param neutral
+	 *            the population of neutral agents that this agent should be
+	 *            separated from (not collide with).
+	 * @return a vector with the force that this agent feels from other neutral
+	 *         agents in order not to collide with them.
+	 *         <p>
+	 *         Warning! Not optimal for linked-lists, due to O(n) complexity of
+	 *         linked list get(n) method. TODO: Special method for linked list
+	 *         using collection.iterator(), hasNext() & next().
 	 */
-	protected Vector getSeparationForce(List<IPopulation> neutral){
-		//Allocating new object here is ok since its only 1 per method call. //Erik
-		Vector separationForce = new Vector(0,0);
-		//int nVisiblePredators = 0; //Unused?
+	protected Vector getSeparationForce(List<IPopulation> neutral) {
+		// Allocating new object here is ok since its only 1 per method call.
+		// //Erik
+		Vector separationForce = new Vector(0, 0);
+		// int nVisiblePredators = 0; //Unused?
 		IPopulation pop;
 		int popSize = neutral.size();
-		for(int j = 0 ; j < popSize ; j++) {
+		for (int j = 0; j < popSize; j++) {
 			pop = neutral.get(j);
 			int size = pop.getAgents().size();
 			List<IAgent> agents = pop.getAgents();
 			IAgent agent;
-			for(int i = 0; i < size; i++) {
+			for (int i = 0; i < size; i++) {
 				agent = agents.get(i);
-				if(agent != this) {
+				if (agent != this) {
 					Position p = agent.getPosition();
 					double distance = getPosition().getDistance(p);
-					if(distance<=INTERACTION_RANGE){ //If neutral is in vision range for prey
+					if (distance <= INTERACTION_RANGE) { // If neutral is in
+															// vision range for
+															// prey
 						/*
 						 * Create a vector that points away from the neutral.
-						 * TODO: Remove the "new Vector" and replace with doubles. This will be called alot of times.
-						 * Low level programming is crucial.
+						 * TODO: Remove the "new Vector" and replace with
+						 * doubles. This will be called alot of times. Low level
+						 * programming is crucial.
 						 */
-						Vector newForce = new Vector(this.getPosition(),p);
-						
+						Vector newForce = new Vector(this.getPosition(), p);
+
 						/*
-						 * Add this vector to the separation force, with proportion to how close the neutral agent is.
-						 * Closer agents will affect the force more than those far away. 
+						 * Add this vector to the separation force, with
+						 * proportion to how close the neutral agent is. Closer
+						 * agents will affect the force more than those far
+						 * away.
 						 */
 						double norm = newForce.getNorm();
-						double v = 1/(norm*distance*distance);
+						double v = 1 / (norm * distance * distance);
 						newForce.x = newForce.x * v;
 						newForce.y = newForce.y * v;
 						separationForce.x = separationForce.x + newForce.x;
 						separationForce.y = separationForce.y + newForce.y;
-						
-						//nVisiblePredators++;//Unused?
+
+						// nVisiblePredators++;//Unused?
 					}
 				}
 			}
 		}
-		return separationForce;		
+		return separationForce;
 	}
-	
+
 	/**
-	 * * @author Sebbe
-	 * The environment force is at the moment defined as 1/(wall-constant*(distance to wall)^2).
-	 * The agents feel the forces from the wall directly to the left, right, top and bottom.
-	 * @param dim The dimensions of the rectangular environment.
+	 * * @author Sebbe The environment force is at the moment defined as
+	 * 1/(wall-constant*(distance to wall)^2). The agents feel the forces from
+	 * the wall directly to the left, right, top and bottom.
+	 * 
+	 * @param dim
+	 *            The dimensions of the rectangular environment.
 	 * @return A vector with the force that an agent feel from its environment.
 	 */
-	protected Vector getEnvironmentForce(Dimension dim){
+	protected Vector getEnvironmentForce(Dimension dim) {
 		/*
 		 * The positions below is just an orthogonal projection on to the walls.
 		 */
-		Position xWallLeft = new Position(0,this.getPosition().getY());
-		Position xWallRight = new Position(dim.getWidth(),this.getPosition().getY());
-		Position yWallBottom = new Position(this.getPosition().getX(),0);
-		Position yWallTop = new Position(this.getPosition().getX(),dim.getHeight());
-		
+		Position xWallLeft = new Position(0, this.getPosition().getY());
+		Position xWallRight = new Position(dim.getWidth(), this.getPosition()
+				.getY());
+		Position yWallBottom = new Position(this.getPosition().getX(), 0);
+		Position yWallTop = new Position(this.getPosition().getX(),
+				dim.getHeight());
+
 		/*
-		 * There is a "-1" in the equation just to make it more unlikely that they actually make it to the wall,
-		 * despite the force they feel (can be interpreted as they stop 1 pixel before the wall).
+		 * There is a "-1" in the equation just to make it more unlikely that
+		 * they actually make it to the wall, despite the force they feel (can
+		 * be interpreted as they stop 1 pixel before the wall).
 		 */
-		Vector environmentForce = new Vector(0,0);
+		Vector environmentForce = new Vector(0, 0);
 		double xWallLeftForce = 0;
 		double xWallRightForce = 0;
 		double yWallBottomForce = 0;
 		double yWallTopForce = 0;
-		
+
 		/*
 		 * Only interacts with walls that are closer than INTERACTION_RANGE.
 		 */
 		double leftWallDistance = this.getPosition().getDistance(xWallLeft);
-		if(leftWallDistance<=INTERACTION_RANGE){
-			xWallLeftForce = 1/Math.pow((leftWallDistance-1.0)/WALL_CONSTANT,2);
+		if (leftWallDistance <= INTERACTION_RANGE) {
+			xWallLeftForce = 1 / Math.pow((leftWallDistance - 1.0)
+					/ WALL_CONSTANT, 2);
 		}
-		
+
 		double rightWallDistance = this.getPosition().getDistance(xWallRight);
-		if(rightWallDistance<=INTERACTION_RANGE){
-			xWallRightForce = -1/Math.pow((rightWallDistance-1.0)/WALL_CONSTANT,2);
+		if (rightWallDistance <= INTERACTION_RANGE) {
+			xWallRightForce = -1
+					/ Math.pow((rightWallDistance - 1.0) / WALL_CONSTANT, 2);
 		}
-		
+
 		double bottomWallDistance = this.getPosition().getDistance(yWallBottom);
-		if(bottomWallDistance<=INTERACTION_RANGE){
-			yWallBottomForce = 1/Math.pow((bottomWallDistance-1.0)/WALL_CONSTANT,2);
+		if (bottomWallDistance <= INTERACTION_RANGE) {
+			yWallBottomForce = 1 / Math.pow((bottomWallDistance - 1.0)
+					/ WALL_CONSTANT, 2);
 		}
-		
+
 		double topWallDistance = this.getPosition().getDistance(yWallTop);
-		if(topWallDistance<=INTERACTION_RANGE){
-			yWallBottomForce = yWallTopForce = -1/Math.pow((topWallDistance-1.0)/WALL_CONSTANT,2);
+		if (topWallDistance <= INTERACTION_RANGE) {
+			yWallBottomForce = yWallTopForce = -1
+					/ Math.pow((topWallDistance - 1.0) / WALL_CONSTANT, 2);
 		}
-		
+
 		/*
-		 * Add the forces from left and right to form the total force from walls in x-axis.
-		 * Add the forces from top and bottom to form the total force from walls in y-axis.
-		 * Create a force vector of the forces.
+		 * Add the forces from left and right to form the total force from walls
+		 * in x-axis. Add the forces from top and bottom to form the total force
+		 * from walls in y-axis. Create a force vector of the forces.
 		 */
 		double xForce = (xWallLeftForce + xWallRightForce);
 		double yForce = (yWallBottomForce + yWallTopForce);
 		environmentForce.setVector(xForce, yForce);
-		
+
 		return environmentForce;
 	}
 
-	
 	/**
 	 * Create a new IAgent from the raw parameters.
+	 * 
 	 * @return
 	 */
 	public static IAgent createFromFile(String input) {
 		String[] inputArray = input.split(";");
 		String name = inputArray[0];
-		Position pos = new Position(Integer.parseInt(inputArray[1]), Integer.parseInt(inputArray[2]));
-		Color c = new Color(Integer.parseInt(inputArray[3]), Integer.parseInt(inputArray[4]), Integer.parseInt(inputArray[5]));
+		Position pos = new Position(Integer.parseInt(inputArray[1]),
+				Integer.parseInt(inputArray[2]));
+		Color c = new Color(Integer.parseInt(inputArray[3]),
+				Integer.parseInt(inputArray[4]),
+				Integer.parseInt(inputArray[5]));
 		int width = Integer.parseInt(inputArray[6]);
 		int height = Integer.parseInt(inputArray[7]);
-		Vector v = new Vector(Integer.parseInt(inputArray[8]), Integer.parseInt(inputArray[9]));
-		int maxSpeed        = Integer.parseInt(inputArray[10]);
-		int visionRange     = Integer.parseInt(inputArray[11]);
+		Vector v = new Vector(Integer.parseInt(inputArray[8]),
+				Integer.parseInt(inputArray[9]));
+		int maxSpeed = Integer.parseInt(inputArray[10]);
+		int visionRange = Integer.parseInt(inputArray[11]);
 		int maxAcceleration = Integer.parseInt(inputArray[12]);
-		
-		AbstractAgent ab = new AbstractAgent(name, pos, c, width, height, v, maxSpeed, visionRange, maxAcceleration) {
-			
+
+		AbstractAgent ab = new AbstractAgent(name, pos, c, width, height, v,
+				maxSpeed, visionRange, maxAcceleration) {
+
 			@Override
 			public List<IAgent> reproduce(IAgent agent, int populationSize) {
 				return null;
 			}
-			
+
 			@Override
 			public void calculateNextPosition(List<IPopulation> predators,
-					List<IPopulation> preys, List<IPopulation> neutral, Dimension dim) {
-				
+					List<IPopulation> preys, List<IPopulation> neutral,
+					Dimension dim) {
+
 			}
 		};
 		return ab;
 	}
-	
+
 	@Override
 	public String toBinaryString() {
 		StringBuilder sb = new StringBuilder();
--- src/chalmers/dax021308/ecosystem/model/agent/GrassAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/GrassAgent.java
@@ -16,17 +16,16 @@ import chalmers.dax021308.ecosystem.model.util.Vector;
  * 
  */
 public class GrassAgent extends AbstractAgent {
-	private double reproduceDelay = 0;
 	private final Dimension gridDimension;
 	private static final double REPRODUCTION_RATE = 0.004;
-	
+
 	public GrassAgent(String name, Position pos, Color color, int width,
 			int height, Vector velocity, double maxSpeed,
 			Dimension gridDimension) {
 		super(name, pos, color, width, height, velocity, maxSpeed, 0, 0);
 		this.gridDimension = gridDimension;
 	}
-	
+
 	public GrassAgent(String name, Position pos, Color color, int width,
 			int height, Vector velocity, double maxSpeed,
 			Dimension gridDimension, int capacity) {
@@ -47,24 +46,49 @@ public class GrassAgent extends AbstractAgent {
 
 	@Override
 	public List<IAgent> reproduce(IAgent agent, int populationSize) {
-		double repr = Math.random() * 100000; // Random number, 'randomly' chosen
-		// Do the reproducing if repr < reproduceDelay, if not increase
-		// reproduceDelay to increase the chance of reproducing next iteration
-		double popSize = (double)populationSize;
-		double cap = (double)capacity;
-		if (Math.random() < REPRODUCTION_RATE*(1.0-popSize/cap)) {
+		double popSize = (double) populationSize;
+		double cap = (double) capacity;
+		if (Math.random() < REPRODUCTION_RATE * (1.0 - popSize / cap)) {
 			List<IAgent> spawn = new ArrayList<IAgent>();
 			IAgent a = new GrassAgent(name,
-					calculateNewPosition(getPosition()), color, 5, 5, velocity,
+					getSpawnPosition(), color, 5, 5, velocity,
 					maxSpeed, gridDimension, capacity);
-			reproduceDelay = 0;
 			spawn.add(a);
 			return spawn;
-		} else {
-			reproduceDelay++;
-			return null;
 		}
+		return null;
 
+	}
+
+	/**
+	 * Finds a new position close to the current position
+	 * 
+	 * @return The position found
+	 */
+	private Position getSpawnPosition() {
+		Position pos;
+		Vector v = new Vector(5, 0);
+		// Find a position which lies 5 pixels away from the current position in
+		// a random direction
+		do {
+			pos = new Position(position);
+			v.rotate(Math.random() * 2 * Math.PI);
+			pos.addVector(v);
+
+		} while (!legitPos(pos));
+		return pos;
+	}
+
+	/**
+	 * Checks if a Position is legit, i.e it is inside the view
+	 * 
+	 * @param pos
+	 *            the Position to check
+	 * @return True if pos lies inside the view, otherwise returns false
+	 */
+	private boolean legitPos(Position pos) {
+		return (pos.getX() > 0 && pos.getX() < gridDimension.getWidth()
+				&& pos.getY() > 0 && pos.getY() < gridDimension.getHeight());
 	}
 
 	/**
@@ -74,18 +98,11 @@ public class GrassAgent extends AbstractAgent {
 	 *            the initial position
 	 * @return a 'legit' position, aka one that is inside the view
 	 */
-	private Position calculateNewPosition(Position p) {
-		// Randomly chooses a direction and adds a vector to it to ensure it
-		// isn't on top of the current position, and then weighs in the
-		// environmentforce
-		/*Position(p);
-		Vector v = new Vector(5, 5);
-		v.rotate(Math.random() * 2 * Math.PI).add(
-				getEnvironmentForce(gridDimension).multiply(100));
-		pos.addVector(v);*/
-		//random position
-		Position pos = new Position(gridDimension.getWidth()
-				* Math.random(), gridDimension.getHeight() * Math.random());
+	private Position calculateNewPosition() {
+		// random position
+		// TODO Remove when getSpawnPosition works
+		Position pos = new Position(gridDimension.getWidth() * Math.random(),
+				gridDimension.getHeight() * Math.random());
 		return pos;
 	}
 }
