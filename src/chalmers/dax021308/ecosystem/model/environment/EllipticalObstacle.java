fc77aa6f834d3fb2d81ffc7d996e253cecda97c3
Sebastian Ånerud <anerud@student.chalmers.se>
2013-03-15 16:28:41 +0100

    Working on the Elliptical obstacle. Still have bugs in it.

--- src/chalmers/dax021308/ecosystem/model/environment/EllipticalObstacle.java
+++ src/chalmers/dax021308/ecosystem/model/environment/EllipticalObstacle.java
@@ -5,6 +5,9 @@ import chalmers.dax021308.ecosystem.model.util.Vector;
 
 public class EllipticalObstacle extends AbstractObstacle{
 	
+	private static final double e = 0.001;
+	private static final double nStep = 4;
+	
 	private double width;
 	private double height;
 	
@@ -15,9 +18,38 @@ public class EllipticalObstacle extends AbstractObstacle{
 	}
 	@Override
 	public Position closestBoundary(Position p) {
-		
-		
-		return p;
+		Position agentPos = new Position(p.getX()-this.position.getX(), p.getY()-this.position.getY());
+		Position bestPos = recursiveBoundarySearch(agentPos, Double.MAX_VALUE, 0, Math.PI/2);
+		bestPos.setPosition(bestPos.getX()+this.position.getX(), bestPos.getY()+this.position.getY());
+		return bestPos;
 	}
 
+	private Position recursiveBoundarySearch(Position agentPos, double lastDistance ,double startAngle, double step){
+	
+		Position elipPos = new Position(Double.MAX_VALUE, Double.MAX_VALUE);
+		Position bestPos = elipPos;
+		Position nextBestPos = elipPos;
+		double bestI = 0;
+		double nextBestI = 0;
+		for(double i=0; i<nStep; i++){
+			elipPos = new Position(width*Math.cos(startAngle+i*step), height*Math.sin(startAngle+i*step));
+			if(elipPos.getDistance(agentPos)<bestPos.getDistance(agentPos)){
+				bestPos = elipPos;
+				nextBestI = bestI;
+				bestI = i;
+			} else if (elipPos.getDistance(agentPos) < nextBestPos.getDistance(agentPos)) {
+				nextBestPos = elipPos;
+				nextBestI = i;
+			}
+		}
+		
+		System.out.println(nextBestI-bestI);
+		double distance = elipPos.getDistance(agentPos);
+		if(lastDistance-distance<e){
+			return elipPos;
+		} else {
+			return recursiveBoundarySearch(agentPos, distance, startAngle+bestI*step, (nextBestI-bestI)*(step/nStep));
+		}
+		
+	}
 }
---
+++ src/chalmers/dax021308/ecosystem/model/environment/EllipticalObstacleTest.java
@@ -0,0 +1,56 @@
+package chalmers.dax021308.ecosystem.model.environment;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import chalmers.dax021308.ecosystem.model.util.Position;
+
+public class EllipticalObstacleTest extends JPanel {
+	
+	public static List<Position> positions;
+	
+	public static void main(String[] args) {
+	
+		EllipticalObstacleTest test = new EllipticalObstacleTest();
+		positions = new ArrayList<Position>();
+		EllipticalObstacle eo = new EllipticalObstacle(100, 50, new Position(200,200));
+		for (int i=0; i<500;i++){
+			Position pos = new Position(i,0);
+			positions.add(eo.closestBoundary(pos));
+		}
+		for (int i=0; i<500;i++){
+			Position pos = new Position(500,i);
+			positions.add(eo.closestBoundary(pos));
+		}
+		for (int i=0; i<500;i++){
+			Position pos = new Position(i,500);
+			positions.add(eo.closestBoundary(pos));
+		}
+		for (int i=0; i<500;i++){
+			Position pos = new Position(0,i);
+			positions.add(eo.closestBoundary(pos));
+		}
+		Frame frame = new Frame();
+		frame.add(test);
+		frame.setSize(new Dimension(500,500));
+		test.repaint();
+		frame.setVisible(true);
+		
+	}
+
+	@Override
+    public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		for(int i=0; i < positions.size(); i++){
+			Position p = positions.get(i);
+			g.drawOval((int)p.getX(), (int)p.getY(), 1, 1);
+			System.out.println(p);
+		}
+	}
+
+}
