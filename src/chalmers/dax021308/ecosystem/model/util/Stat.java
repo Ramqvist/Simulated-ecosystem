f78812db3204a4c73bc6da0d5d867b22ba9c1345
Sebastian Ånerud <anerud@student.chalmers.se>
2013-03-15 11:01:00 +0100

    Now one can make a stat object.

--- src/chalmers/dax021308/ecosystem/model/util/Stat.java
+++ src/chalmers/dax021308/ecosystem/model/util/Stat.java
@@ -1,12 +1,55 @@
 package chalmers.dax021308.ecosystem.model.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Sebbe
  * This is a class for statistical calculations.
  */
-public class Stat {
+public class Stat<T extends Number> {
+	
+	private List<T> sample;
+	private double mean;
+	private double sampleVariance;
+	
+	/**
+	 * Creates a new and empty Stat object.
+	 */
+	public Stat(){
+		sample = new ArrayList<T>();
+		mean = 0;
+	}
+	
+	/**
+	 * Add and observation to the Stat sample
+	 * @param obs observation to add.
+	 */
+	public void addObservation(T obs){
+		sample.add(obs);
+		double sampleSize = sample.size();
+		if(sampleSize <= 1){
+			mean = obs.doubleValue();
+		} else {
+			mean = ((mean)*(sampleSize-1)+obs.doubleValue())/sampleSize;
+		}
+	}
+	
+	public double getMean(){
+		return mean;
+	}
+	
+	public double getSampleVariance(){
+		int sampleSize = sample.size();
+		if(sampleSize<2){
+			return 0;
+		}
+		int sum = 0;
+		for(int i=0; i<sampleSize; i++){
+			sum += (sample.get(i).doubleValue()-mean)*(sample.get(i).doubleValue()-mean);
+		}
+		return sum/(sampleSize-1.0);
+	}
 	
 	/**
 	 * Calculates the mean of a sample
