df593eb7bd88b45419922563902239395be643ee
Erik Ramqvist <erikramqvist@gmail.com>
2013-03-17 13:33:38 +0100

    Fixed error, calculating MutialInteractionForce is now done at GPU.

--- src/chalmers/dax021308/ecosystem/model/agent/MutualInteractionForceKernel.java
+++ src/chalmers/dax021308/ecosystem/model/agent/MutualInteractionForceKernel.java
@@ -6,51 +6,57 @@ import com.amd.aparapi.Kernel;
 
 public class MutualInteractionForceKernel extends Kernel {
 	//Only final fields in kernels.
-	public final float[] xPosArray;
-	public final float[] yPosArray;
-	public final float[] xResult;
-	public final float[] yResult;
+	public final double[] xPosArray;
+	public final double[] yPosArray;
+	public final double[] xResult;
+	public final double[] yResult;
 
-	public final float INTERACTION_RANGE;
-	public final float myPosX;
-	public final float myPosY;
+	public final double INTERACTION_RANGE;
+	public final double myPosX;
+	public final double myPosY;
 	
-	public MutualInteractionForceKernel(int size, float INTERACTION_RANGE, float myPosX, float myPosY) {
-		xPosArray = new float[size];
-		yPosArray = new float[size];
+	public MutualInteractionForceKernel(int size, double INTERACTION_RANGE, double myPosX, double myPosY) {
+		xPosArray = new double[size];
+		yPosArray = new double[size];
 		this.INTERACTION_RANGE = INTERACTION_RANGE;
 		this.myPosX = myPosX;
 		this.myPosY = myPosY;
-		xResult = new float[size];
-		yResult = new float[size];
+		xResult = new double[size];
+		yResult = new double[size];
 	}
 
 	@Override
 	public void run() {
-			float mutualInteractionForceX = 0;
-			float mutualInteractionForceY = 0;
-			float xAgentPosition = xPosArray[getGlobalId()];
-			float yAgentPosition = yPosArray[getGlobalId()];
-//			float distance = getDistance(myPosX, myPosY, xAgentPosition, yAgentPosition);
-			float dX = myPosX - xAgentPosition;
-			float dY = myPosY - yAgentPosition;
+			double newForceX = 0;
+			double newForceY = 0;
+			double mutualInteractionForceX = 0;
+			double mutualInteractionForceY = 0;
+			double dX = 0;
+			double dY = 0;
+			int gid = getGlobalId();
+			double xAgentPosition = xPosArray[gid];
+			double yAgentPosition = yPosArray[gid];
+			double v = 0;
+			double norm = 0;
+			double distance = 0;
+//			double distance = getDistance(myPosX, myPosY, xAgentPosition, yAgentPosition);
+			dX = myPosX - xAgentPosition;
+			dY = myPosY - yAgentPosition;
 			//Math library allowed in Aparapi!
-			float distance = (float) Math.sqrt(dX * dX + dY * dY);
-			float Q = 0; // Q is a function of the distance.
+			distance = Math.sqrt(dX * dX + dY * dY);
+			double Q = 0; // Q is a function of the distance.
 			if (distance <= INTERACTION_RANGE) {
 				Q = -20 * (INTERACTION_RANGE - distance);
 			} else {
 				Q = 1;
 			}	
-			float newForceX = 0;
-			float newForceY = 0;
 			newForceX = xAgentPosition - myPosX;
 			newForceY = yAgentPosition - myPosY;
-			float norm = (float) Math.sqrt(Math.pow(newForceX,2)+Math.pow(newForceY,2));
-			float v = Q / (norm * distance);
+			norm = Math.sqrt((newForceX*newForceX)+(newForceY*newForceY));
+			v = Q / (norm * distance);
 			newForceX = newForceX * v;
 			newForceY = newForceY * v;
-			xResult[getGlobalId()] = mutualInteractionForceX + newForceX;
-			yResult[getGlobalId()] = mutualInteractionForceY + newForceY;
+			xResult[gid] = mutualInteractionForceX + newForceX;
+			yResult[gid] = mutualInteractionForceY + newForceY;
 	}	
 }
