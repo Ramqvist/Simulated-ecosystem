4b60ce6b0e173cd6df12012c947d243e1c97f812
Erik Ramqvist <erikramqvist@gmail.com>
2013-04-14 19:48:34 +0200

    Copied logic from deer agent to pig agent for experimenting.

--- src/chalmers/dax021308/ecosystem/model/agent/PigAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/PigAgent.java
@@ -2,8 +2,8 @@ package chalmers.dax021308.ecosystem.model.agent;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import chalmers.dax021308.ecosystem.model.environment.obstacle.IObstacle;
 import chalmers.dax021308.ecosystem.model.population.IPopulation;
@@ -12,65 +12,74 @@ import chalmers.dax021308.ecosystem.model.util.Position;
 import chalmers.dax021308.ecosystem.model.util.Vector;
 
 /**
- * The pig is wide and pink, nuff said.
+ * A basic implementation of the IAgent interface.
  * 
- * @author Erik
- * 
+ * @author Albin
  */
 public class PigAgent extends AbstractAgent {
 
-	private Position reUsedPosition;
-	private Random ran;
-	private double wanderVelocity = 0.3;
-	private boolean hungry;
-
-	private List<IAgent> seperationForceList;
-	private int skips;
-	private int calc;
+	private static final int MAX_ENERGY = 1000;
+	private static final int MAX_LIFE_LENGTH = Integer.MAX_VALUE;
+	private boolean hungry = true;
+	private static final double REPRODUCTION_RATE = 0.1;
+	private boolean willFocusPreys = false;
+	private static final int DIGESTION_TIME = 10;
+	private int digesting = 0;
+	private double STOTTING_RANGE = 20;
+	private double STOTTING_LENGTH = 30;
+	private double STOTTING_COOLDOWN = 150;
+	private double stottingDuration = STOTTING_LENGTH;
+	private double stottingCoolDown = 0;
+	private boolean isAStottingDeer = true;
+	private boolean isStotting = false;
+	private Vector stottingVector = new Vector();
+	private boolean alone;
 
 	public PigAgent(String name, Position p, Color c, int width, int height,
-			Vector velocity, double maxSpeed, double visionRange,
-			double maxAcceleration, Random ran, List<IAgent> seperationForceMap) {
+			Vector velocity, double maxSpeed, double maxAcceleration,
+			double visionRange, boolean groupBehaviour) {
+
 		super(name, p, c, width, height, velocity, maxSpeed, visionRange,
 				maxAcceleration);
-		this.ran = ran;
-		this.seperationForceList = seperationForceMap;
+		this.energy = MAX_ENERGY;
+		this.groupBehaviour = groupBehaviour;
+
 	}
 
-	/*
-	 * @Override public void calculateNextPosition(List<IPopulation> predators,
-	 * List<IPopulation> preys, List<IPopulation> neutral, Dimension dim) {
-	 * /*if(reUsedPosition == null) { reUsedPosition = new Position(position); }
-	 * nextPosition = reUsedPosition; int predatorsSize = predators.size();
-	 * List<IAgent> predatorPop; boolean hasNearbyPredator = false; for(int i =
-	 * 0 ; i < predatorsSize ; i++) { predatorPop =
-	 * predators.get(i).getAgents(); int predatorPopSize = predatorPop.size();
-	 * IAgent pred; Position p; for(int j = 0 ; j < predatorPopSize; j++) { pred
-	 * = predatorPop.get(j); p = pred.getPosition(); double distance =
-	 * p.getDistance(position); if(distance < visionRange) { if(p.getX() >
-	 * position.getX()) { velocity.x = -maxAcceleration; } else { velocity.x =
-	 * maxAcceleration; } if(p.getY() > position.getX()) { velocity.y =
-	 * -maxAcceleration; } else { velocity.y = maxAcceleration; }
-	 * hasNearbyPredator = true; } } } if(!hasNearbyPredator) {
-	 * if(ran.nextBoolean()) { velocity.x = wanderVelocity; velocity.y =
-	 * -wanderVelocity; } else if(ran.nextBoolean()) { velocity.x =
-	 * -wanderVelocity; velocity.y = wanderVelocity; } }
-	 * 
-	 * Vector separationForce = getSeparationForce(neutral); velocity.x =
-	 * velocity.x * 1000 + separationForce.x * 1000 + ran.nextDouble() * 10;
-	 * velocity.y = velocity.y * 1000 + separationForce.y * 1000 +
-	 * ran.nextDouble() * 10; Log.v(velocity.toString()); nextPosition = new
-	 * Position(position.getX() + separationForce.x, position.getY() +
-	 * separationForce.x); Log.v(nextPosition.toString()); }
-	 */
-
 	@Override
-	public void updatePosition() {
-		this.reUsedPosition = position;
-		this.position = nextPosition;
+	public List<IAgent> reproduce(IAgent agent, int populationSize,
+			List<IObstacle> obstacles, IShape shape, Dimension gridDimension) {
+		if (hungry)
+			return null;
+		else {
+			hungry = true;
+			List<IAgent> spawn = new ArrayList<IAgent>();
+			if (Math.random() < REPRODUCTION_RATE) {
+				Position pos;
+				do {
+					double xSign = Math.signum(-1 + 2 * Math.random());
+					double ySign = Math.signum(-1 + 2 * Math.random());
+					double newX = this.getPosition().getX() + xSign
+							* (0.001 + 0.001 * Math.random());
+					double newY = this.getPosition().getY() + ySign
+							* (0.001 + 0.001 * Math.random());
+					pos = new Position(newX, newY);
+				} while (!shape.isInside(gridDimension, pos));
+				IAgent child = new PigAgent(name, pos, color, width, height,
+						new Vector(velocity), maxSpeed, maxAcceleration,
+						visionRange, groupBehaviour);
+				spawn.add(child);
+
+			}
+			return spawn;
+		}
 	}
 
 	/**
+	 * Calculates the next position of the agent depending on the forces that
+	 * affects it. Note: The next position is not set until updatePosition() is
+	 * called.
+	 * 
 	 * @author Sebbe
 	 */
 	@Override
@@ -78,213 +87,190 @@ public class PigAgent extends AbstractAgent {
 			List<IPopulation> preys, List<IPopulation> neutral,
 			Dimension gridDimension, IShape shape, List<IObstacle> obstacles) {
 
-		Vector predatorForce = getPredatorForce(predators);
-		Vector separationForce = getEriksOptimeradeSeparationForce(neutral);
-		// Vector separationForce = getSeparationForce(neutral);
-		// Vector separationForce = new Vector();
-		Vector environmentForce = getEnvironmentForce(gridDimension, shape, position);
-		Vector preyForce = getPreyForce(preys);
-
-		/*
-		 * Sum the forces from walls, predators and neutral to form the
-		 * acceleration force. If the acceleration exceeds maximum acceleration
-		 * --> scale it to maxAcceleration, but keep the correct direction of
-		 * the acceleration.
-		 */
-		Vector acceleration = environmentForce.multiply(100)
-				.add(predatorForce.multiply(3))
-				.add(separationForce.multiply(10).add(preyForce));
-		double accelerationNorm = acceleration.getNorm();
-		if (accelerationNorm > maxAcceleration) {
-			acceleration.multiply(maxAcceleration / accelerationNorm);
-		}
-
-		/*
-		 * The new velocity is then just: v(t+dt) = (v(t)+a(t+1)*dt)*decay,
-		 * where dt = 1 in this case. There is a decay that says if they are not
-		 * affected by any force, they will eventually stop. If speed exceeds
-		 * maxSpeed --> scale it to maxSpeed, but keep the correct direction.
-		 */
-		Vector newVelocity = this.getVelocity().add(acceleration)
-				.multiply(VELOCITY_DECAY);
-		double speed = newVelocity.getNorm();
-		if (speed > maxSpeed) {
-			newVelocity.multiply(maxSpeed / speed);
-		}
-
-		this.setVelocity(newVelocity);
-		if (reUsedPosition == null) {
-			reUsedPosition = Position.positionPlusVector(position, velocity);
-			nextPosition = reUsedPosition;
+		updateNeighbourList(neutral, preys, predators);
+		Vector predatorForce = getPredatorForce();
+		if (digesting > 0 && alone) {
+			digesting--;
 		} else {
-			reUsedPosition.setPosition(position.getX() + velocity.x,
-					position.getY() + velocity.y);
-			nextPosition = reUsedPosition;
-		}
-	}
-
-	/**
-	 * @author Sebbe
-	 * @param neutral
-	 *            the population of neutral agents that this agent should be
-	 *            separated from (not collide with).
-	 * @return a vector with the force that this agent feels from other neutral
-	 *         agents in order not to collide with them.
-	 *         <p>
-	 *         Warning! Not optimal for linked-lists, due to O(n) complexity of
-	 *         linked list get(n) method. TODO: Special method for linked list
-	 *         using collection.iterator(), hasNext() & next().
-	 */
-	protected Vector getEriksOptimeradeSeparationForce(List<IPopulation> neutral) {
-		// Allocating new object here is ok since its only 1 per method call.
-		// //Erik
-		Vector separationForce = new Vector(0, 0);
-		IPopulation pop;
-		int popSize = neutral.size();
-		skips = 0;
-		calc = 0;
-		for (int j = 0; j < popSize; j++) {
-			pop = neutral.get(j);
-			int size = pop.getAgents().size();
-			List<IAgent> agents = pop.getAgents();
-			IAgent agent;
-			for (int i = 0; i < size; i++) {
-				agent = agents.get(i);
-				if (agent != this && agent != null) {
-					if (seperationForceList.contains(agent)) {
-						++skips;
-					} else {
-						++calc;
-						Position p = agent.getPosition();
-						double distance = getPosition().getDistance(p);
-						if (distance <= INTERACTION_RANGE) { // If neutral is in
-																// vision range
-																// for prey
-							/*
-							 * Create a vector that points away from the
-							 * neutral. TODO: Remove the "new Vector" and
-							 * replace with doubles. This will be called alot of
-							 * times. Low level programming is crucial.
-							 */
-							Vector newForce = new Vector(this.getPosition(), p);
-
-							/*
-							 * Add this vector to the separation force, with
-							 * proportion to how close the neutral agent is.
-							 * Closer agents will affect the force more than
-							 * those far away.
-							 */
-							double norm = newForce.getNorm();
-							double v = 1 / (norm * distance * distance);
-							newForce.x = newForce.x * v;
-							newForce.y = newForce.y * v;
-							separationForce.x = separationForce.x + newForce.x;
-							separationForce.y = separationForce.y + newForce.y;
-
-						}
-					}
-				}
+			Vector mutualInteractionForce = new Vector();
+			Vector forwardThrust = new Vector();
+			Vector arrayalForce = new Vector();
+			if (groupBehaviour) {
+				mutualInteractionForce = mutualInteractionForce(
+						neutralNeighbours, position);
+				forwardThrust = forwardThrust(velocity);
+				arrayalForce = arrayalForce(velocity, neutralNeighbours, position);
 			}
-			seperationForceList.add(this);
+
+			Vector environmentForce = getEnvironmentForce(gridDimension, shape,
+					position);
+			Vector obstacleForce = getObstacleForce(obstacles, position);
+
+			/*
+			 * Sum the forces from walls, predators and neutral to form the
+			 * acceleration force. If the acceleration exceeds maximum
+			 * acceleration --> scale it to maxAcceleration, but keep the
+			 * correct direction of the acceleration.
+			 */
+			Vector acceleration;
+			if (isAStottingDeer && isStotting) {
+				acceleration = predatorForce;
+			} else {
+				acceleration = predatorForce.multiply(5)
+						.add(mutualInteractionForce).add(forwardThrust)
+						.add(arrayalForce);
+				// if (alone) {
+				Vector preyForce = getPreyForce();
+				acceleration.add(preyForce.multiply(5 * (1 - energy
+						/ MAX_ENERGY)));
+			}
+			// }
+			double accelerationNorm = acceleration.getNorm();
+			if (accelerationNorm > maxAcceleration) {
+				acceleration.multiply(maxAcceleration / accelerationNorm);
+			}
+
+			acceleration.add(environmentForce).add(obstacleForce);
+
+			/*
+			 * The new velocity is then just: v(t+dt) = (v(t)+a(t+1)*dt)*decay,
+			 * where dt = 1 in this case. There is a decay that says if they are
+			 * not affected by any force, they will eventually stop. If speed
+			 * exceeds maxSpeed --> scale it to maxSpeed, but keep the correct
+			 * direction.
+			 */
+			Vector newVelocity = Vector.addVectors(this.getVelocity(),
+					acceleration);
+			newVelocity.multiply(VELOCITY_DECAY);
+			double speed = newVelocity.getNorm();
+			if (speed > maxSpeed) {
+				newVelocity.multiply(maxSpeed / speed);
+			}
+			// if (alone) {
+			// newVelocity.multiply(0.9);
+			// }
+			this.setVelocity(newVelocity);
+
+			/* Reusing the same position object, for less heap allocations. */
+			// if (reUsedPosition == null) {
+			nextPosition = Position.positionPlusVector(position, velocity);
+			// } else {
+			// nextPosition.setPosition(reUsedPosition.setPosition(position.getX()
+			// + velocity.x, position.getY() + velocity.y));
+			// }
 		}
-		// Log.v("SKips " + skips);
-		// Log.v("Calculations: " + calc);
-
-		return separationForce;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = result + position.hashCode();
-		return result;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-
-		return true;
 	}
 
 	/**
-	 * @author Henrik
-	 * @param preys
-	 *            The list of preys to eat
 	 * @return returns The force the preys attracts the agent with
-	 * 
-	 * @author Erik, Method optimized for ArrayList
+	 * @author Sebastian/Henrik
 	 */
-	private Vector getPreyForce(List<IPopulation> preys) {
+	private Vector getPreyForce() {
+		if (willFocusPreys && focusedPrey != null && focusedPrey.isAlive()) {
+			Position p = focusedPrey.getPosition();
+			double distance = getPosition().getDistance(p);
+			if (distance <= EATING_RANGE) {
+				if (focusedPrey.tryConsumeAgent()) {
+					focusedPrey = null;
+					hungry = false;
+					energy = MAX_ENERGY;
+					digesting = DIGESTION_TIME;
+				}
+			} else {
+				return new Vector(focusedPrey.getPosition(), position);
+			}
+		}
+
 		Vector preyForce = new Vector(0, 0);
-		IPopulation pop;
-		int preySize = preys.size();
-		for (int j = 0; j < preySize; j++) {
-			pop = preys.get(j);
-			List<IAgent> agents = pop.getAgents();
-			int agentSize = agents.size();
-			for (int i = 0; i < agentSize; i++) {
-				IAgent a = agents.get(i);
-				Position p = a.getPosition();
-				double distance = getPosition().getDistance(p);
-				if (distance <= visionRange) {
-					if (distance <= INTERACTION_RANGE) {
-						// Food found, let's eat it and make some reproducing
-						// possible
-						pop.addToRemoveList(a);
+		IAgent closestFocusPrey = null;
+		int preySize = preyNeighbours.size();
+		for (int i = 0; i < preySize; i++) {
+			IAgent a = preyNeighbours.get(i);
+			Position p = a.getPosition();
+			double distance = getPosition().getDistance(p);
+			if (distance <= visionRange) {
+				if (distance <= EATING_RANGE) {
+					if (a.tryConsumeAgent()) {
 						hungry = false;
-					} else {
-						Vector newForce = new Vector(p, getPosition());
-						double norm = newForce.getNorm();
-						preyForce.add(newForce.multiply(1 / (norm * distance)));
+						energy = MAX_ENERGY;
+						digesting = DIGESTION_TIME;
 					}
+				} else if (willFocusPreys && distance <= FOCUS_RANGE) {
+					if (closestFocusPrey != null && a.isAlive()) {
+						if (closestFocusPrey.getPosition().getDistance(
+								this.position) > a.getPosition().getDistance(
+								this.position)) {
+							closestFocusPrey = a;
+						}
+					} else {
+						closestFocusPrey = a;
+					}
+				} else if (closestFocusPrey == null) {
+					/*
+					 * Create a vector that points towards the prey.
+					 */
+					Vector newForce = new Vector(p, getPosition());
+
+					/*
+					 * Add this vector to the prey force, with proportion to how
+					 * close the prey is. Closer preys will affect the force
+					 * more than those far away.
+					 */
+					double norm = newForce.getNorm();
+					preyForce.add(newForce.multiply(1 / (norm * distance)));
 				}
 			}
 		}
+
+		double norm = preyForce.getNorm();
+		if (norm != 0) {
+			preyForce.multiply(maxAcceleration / norm);
+		}
+
+		if (willFocusPreys && closestFocusPrey != null) {
+			focusedPrey = closestFocusPrey;
+			return new Vector(focusedPrey.getPosition(), position);
+		}
+
 		return preyForce;
 	}
 
 	/**
-	 * @author Sebbe "Predator Force" is defined as the sum of the vectors
-	 *         pointing away from all the predators in vision, weighted by the
-	 *         inverse of the distance to the predators, then normalized to have
-	 *         unit norm. Can be interpreted as the average sum of forces that
-	 *         the agent feels, weighted by how close the source of the force
-	 *         is.
+	 * "Predator Force" is defined as the sum of the vectors pointing away from
+	 * all the predators in vision, weighted by the inverse of the distance to
+	 * the predators, then normalized to have unit norm. Can be interpreted as
+	 * the average sum of forces that the agent feels, weighted by how close the
+	 * source of the force is.
 	 * 
-	 * @author Erik, Method optimized for ArrayList
+	 * @author Sebbe
 	 */
-	private Vector getPredatorForce(List<IPopulation> predators) {
+	private Vector getPredatorForce() {
 		Vector predatorForce = new Vector(0, 0);
-		int nVisiblePredators = 0;
-		IPopulation pop;
-		int popSize = predators.size();
-		for (int i = 0; i < popSize; i++) {
-			pop = predators.get(i);
-			List<IAgent> agents = pop.getAgents();
-			int agentSize = agents.size();
+		if (isAStottingDeer && isStotting) {
+			stottingDuration--;
+			if (stottingDuration <= 0) {
+				isStotting = false;
+			}
+			return stottingVector;
+		} else {
+			boolean predatorClose = false;
+			int predSize = predNeighbours.size();
 			IAgent predator;
-			for (int j = 0; j < agentSize; j++) {
-				predator = agents.get(j);
+			for (int i = 0; i < predSize; i++) {
+				predator = predNeighbours.get(i);
 				Position p = predator.getPosition();
 				double distance = getPosition().getDistance(p);
 				if (distance <= visionRange) { // If predator is in vision range
 												// for prey
+
 					/*
 					 * Create a vector that points away from the predator.
 					 */
 					Vector newForce = new Vector(this.getPosition(), p);
+
+					if (isAStottingDeer && distance <= STOTTING_RANGE) {
+						predatorClose = true;
+					}
 
 					/*
 					 * Add this vector to the predator force, with proportion to
@@ -293,29 +279,51 @@ public class PigAgent extends AbstractAgent {
 					 */
 					double norm = newForce.getNorm();
 					predatorForce.add(newForce.multiply(1 / (norm * distance)));
-					nVisiblePredators++;
 				}
 			}
-		}
 
-		if (nVisiblePredators == 0) { // No predators near --> Be unaffected
-			predatorForce.setVector(0, 0);
-		} else { // Else set the force depending on visible predators and
-					// normalize it to maxAcceleration.
 			double norm = predatorForce.getNorm();
-			predatorForce.multiply(maxAcceleration / norm);
-		}
+			if (norm <= 0) { // No predators near --> Be unaffected
+				alone = true;
+			} else { // Else set the force depending on visible predators and
+						// normalize it to maxAcceleration.
+				predatorForce.multiply(maxAcceleration / norm);
+				alone = false;
+			}
 
+			if (isAStottingDeer && stottingCoolDown <= 0 && predatorClose) {
+				isStotting = true;
+				stottingCoolDown = STOTTING_COOLDOWN;
+				stottingDuration = STOTTING_LENGTH;
+				double newX = 0;
+				double newY = 0;
+				if (Math.random() < 0.5) {
+					newX = 1;
+					newY = -predatorForce.getX() / predatorForce.getY();
+				} else {
+					newY = 1;
+					newX = -predatorForce.getY() / predatorForce.getX();
+				}
+				stottingVector.setVector(newX, newY);
+				stottingVector.multiply(predatorForce.getNorm()
+						/ stottingVector.getNorm());
+				stottingVector.add(predatorForce.multiply(-0.5));
+				return stottingVector;
+			}
+
+		}
 		return predatorForce;
 	}
 
+	/**
+	 * This also decreases the deer's energy.
+	 */
 	@Override
-	public List<IAgent> reproduce(IAgent agent, int populationSize, List<IObstacle> obstacles, IShape shape, Dimension gridDimension) {
-		return null;
+	public void updatePosition() {
+		super.updatePosition();
+		this.energy--;
+		stottingCoolDown--;
+		if (energy == 0 || lifeLength > MAX_LIFE_LENGTH)
+			isAlive = false;
 	}
-
-	public IAgent reproduceOne(int populationSize) {
-		return null;
-	}
-
 }
