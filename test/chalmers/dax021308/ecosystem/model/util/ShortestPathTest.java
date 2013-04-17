e76b1316188cebabf40928c1a997b571fb0dec2a
Erik Ramqvist <erikramqvist@gmail.com>
2013-04-17 10:20:30 +0200

    Updated shortestpath test.

--- test/chalmers/dax021308/ecosystem/model/util/ShortestPathTest.java
+++ test/chalmers/dax021308/ecosystem/model/util/ShortestPathTest.java
@@ -37,10 +37,25 @@ public class ShortestPathTest {
 		}
 		JPSPathfinder finder = new JPSPathfinder(obsList, shape, dim);
 		List<Position> path = finder.getShortestPath(start, end);
-//		Log.v(path);
 		Position lastPostion = path.get(path.size() - 1);
 		Position firstPosition = path.get(0);
-//		Log.v(lastPostion);
+		Assert.assertTrue(lastPostion.equals(end));
+		Assert.assertTrue(firstPosition.equals(start));
+	}
+	
+
+	@Test
+	public void shortestPathTest2() {
+		Position end = new Position(125, 320);
+		Position start = new Position(765, 565);
+		Dimension dim = new Dimension(1000, 1000);
+		if(AbstractObstacle.isInsideObstacleList(obsList, start) || AbstractObstacle.isInsideObstacleList(obsList, end)) {
+			Log.e("Either positions is inside obstacle.");
+		}
+		JPSPathfinder finder = new JPSPathfinder(obsList, shape, dim);
+		List<Position> path = finder.getShortestPath(start, end);
+		Position lastPostion = path.get(path.size() - 1);
+		Position firstPosition = path.get(0);
 		Assert.assertTrue(lastPostion.equals(end));
 		Assert.assertTrue(firstPosition.equals(start));
 	}
