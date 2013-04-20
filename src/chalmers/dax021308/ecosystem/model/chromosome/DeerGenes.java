0807d7051d37272cceb9ccc67a44303b18549fa5
gmyra <gmyraistacken@gmail.com>
2013-04-20 18:26:11 +0200

    Moved some stuff

--- src/chalmers/dax021308/ecosystem/model/chromosome/DeerGenes.java
+++ src/chalmers/dax021308/ecosystem/model/chromosome/DeerGenes.java
@@ -1,29 +1,72 @@
 package chalmers.dax021308.ecosystem.model.chromosome;
 
+import java.util.EnumSet;
+
 /**
  * 
  * @author Loanne Berggren
  *
  */
+/*public class DeerGenes extends AbstractGenes<DeerGenes.DG>
+{
+	protected enum DG{
+		STOTTING(0),
+		FEMALE(1),
+		JUNK(3);
+
+		private int value;
+
+		public int getValue() {
+			return value;
+		}
+
+		private DG(int value) {
+			this.value = value;
+		}
+		private static final int numberOfGenes = DG.values().length;
+
+
+	}
+	//private static final int numberOfGenes = DG.values().length;
+
+	private DeerGenes() {
+		super(DG.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public AbstractGenes<?> genesFactory() {
+		// TODO Auto-generated method stub
+		return new DeerGenes();
+	}
+
+}
+
+*/
+
+
+//public enum DeerGenes implements IGenes<DeerGenes>
 public enum DeerGenes
 {
 	STOTTING(0),
-	FEMALE(1),
-	JUNK(3);
+	FEMALE(1)
+	// To add gene: genename(#)
+	// ex JUNK(2)
+	;
 	
-	private int value;
+	private int index;
+	private static final int numberOfGenes = DeerGenes.values().length;
 	
-	public int getValue() {
-		return value;
+	public int getIndex() {
+		return index;
 	}
 	
-	private static final int numberOfGenes = 3;
-	
-	public static int getNumberOfGenes() {
+	public static int getNumberOfGenes2() {
 		return numberOfGenes;
 	}
 	
-	private DeerGenes(int value) {
-		this.value = value;
+	private DeerGenes(int index) {
+		this.index = index;
 	}
+
 }
--- src/chalmers/dax021308/ecosystem/model/chromosome/DeerGenome.java
+++ src/chalmers/dax021308/ecosystem/model/chromosome/DeerGenome.java
@@ -1,102 +1,70 @@
 package chalmers.dax021308.ecosystem.model.chromosome;
 
+public class DeerGenome extends AbstractGenome<DeerGenes> {
 
-/**
- * 
- * @author Loanne Berggren
- *
- */
-public class DeerGenome extends AbstractGenome<DeerGenes>{
-	
-	private BitSetChromosome chromosome;
-	// Random value.
-	private final double MUTATION_PROBABILITY = 0.1;
+	private static double MUTATION_PROBABILITY = 0.1;
 	
 	/**
 	 * Creates a new DeerGenome.
 	 * Number of genes depends on DeerGenes.
 	 * All genes are set to false initially. 
 	 * Use {@link #setGene(DeerGenes, boolean)} to change gene value.
-	 * @see {@link #DeerGenome(boolean, boolean)}
+	 * 
 	 */
-	public DeerGenome() {	
-		this.chromosome = new BitSetChromosome(DeerGenes.getNumberOfGenes(), MUTATION_PROBABILITY);
-	}
-	
-	/**
-	 * Creates a new DeerGenome.
-	 * @param stotting
-	 * @param female
-	 */
-	public DeerGenome(boolean stotting, boolean female) {	
-		this();
-		this.setGene(DeerGenes.STOTTING, stotting);
-		this.setGene(DeerGenes.FEMALE, female);
+	public DeerGenome() {
+		super(new BitSetChromosome(DeerGenes.getNumberOfGenes2(), MUTATION_PROBABILITY));
 	}
 	
 	/*
 	 * Used privately only.
 	 */
-	private DeerGenome(BitSetChromosome chromosome) {
-		this.chromosome = chromosome;
+	private DeerGenome(IChromosome chromosome) {
+		super(chromosome);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * 
 	 **/
 	@Override
-	public AbstractGenome<DeerGenes> mateWithMutation(AbstractGenome<DeerGenes> other){
-		if (other instanceof DeerGenome) {
-			BitSetChromosome nChrom = chromosome.crossChromosomes((BitSetChromosome)other.getChromosome());
-			nChrom.mutateChromosome();
-			AbstractGenome<DeerGenes> newGenome = new DeerGenome(nChrom);
-			return newGenome;
-		}
-		return null;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public AbstractGenome<DeerGenes> onlyMutate(){
-		BitSetChromosome nChrom = (BitSetChromosome) this.chromosome.clone();
-		nChrom.mutateChromosome();
-		AbstractGenome<DeerGenes> newGenome = new DeerGenome(nChrom);	
-		return newGenome;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
 	public boolean isGeneSet(DeerGenes gene) {
-		return chromosome.findGene(gene.getValue());
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Object getChromosome(){
-		return this.chromosome;
+		return this.chromosome.findGene(gene.getIndex());
 	}
 
 	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int numberOfGenes() {
-		return DeerGenes.getNumberOfGenes();
-	}
-
-	/**
-	 * {@inheritDoc}
+	 * 
+	 * @param gene	The gene of type DeerGenes to set.
+	 * @param allele	{@inheritDoc}
 	 */
 	@Override
 	public void setGene(DeerGenes gene, boolean allele) {
-		this.chromosome.setGene(gene.getValue(), allele);
+		this.chromosome.setGene(gene.getIndex(), allele);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object other) {
+		DeerGenome o;
+		if (other instanceof DeerGenome)
+			o = (DeerGenome)other;
+		else return false;
+		if ( !this.chromosome.equals(o.getChromosomes()) )
+			return false;		
+		return true;
+	}
+	
+	/**
+	 * @throws CloneNotSupportedException
+	 */
+	@Override 
+	public final Object clone() throws CloneNotSupportedException {
+	    throw new CloneNotSupportedException();
+	}
+
+	@Override
+	protected IGenome<DeerGenes> genomeFactory(IChromosome chromosome) {
+		return new DeerGenome(chromosome);
+	}
 }
