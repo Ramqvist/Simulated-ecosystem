87c98a5f8b0af16096b3c5b1d7829fe036c4c520
Albin <a.bramstang@gmail.com>
2013-04-21 18:50:31 +0200

    Made wolfs ignore deer when not hungry.

--- src/chalmers/dax021308/ecosystem/model/agent/WolfAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/WolfAgent.java
@@ -5,8 +5,6 @@ import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
 
-import chalmers.dax021308.ecosystem.model.chromosome.AbstractGenome;
-import chalmers.dax021308.ecosystem.model.chromosome.DeerGenes;
 import chalmers.dax021308.ecosystem.model.chromosome.IGenome;
 import chalmers.dax021308.ecosystem.model.chromosome.WolfGenes;
 import chalmers.dax021308.ecosystem.model.environment.obstacle.IObstacle;
@@ -17,8 +15,8 @@ import chalmers.dax021308.ecosystem.model.util.Vector;
 import chalmers.dax021308.ecosystem.model.util.shape.IShape;
 
 /**
- * @author Henrik Its purpose is to hunt down Agentsof lower trophic level in a
- *         simple way
+ * Hunts other agents at lower trophic levels.
+ * @author Henrik 
  */
 public class WolfAgent extends AbstractAgent {
 
@@ -26,10 +24,15 @@ public class WolfAgent extends AbstractAgent {
 	private boolean willFocusPreys = true;
 	private static final int MAX_ENERGY = 1200;
 	private static final int MAX_LIFE_LENGTH = Integer.MAX_VALUE;
-	private static final double REPRODUCTION_RATE = 0.10;
+	private static final double REPRODUCTION_RATE = 0.0005;
 	private static final int DIGESTION_TIME = 50;
+	private static final int PREY_FORCE_WEIGHT = 10;
+	private static final int SPRINT_INTERVALL = 10;
 	private int digesting = 0;
 	private IGenome<WolfGenes> genome;
+	private int preyForceFactor = 0;
+	private boolean isSprinting = false;
+	private int sprintTimer = SPRINT_INTERVALL;
 
 	public WolfAgent(String name, Position position, Color color, int width,
 			int height, Vector velocity, double maxSpeed,
@@ -80,8 +83,11 @@ public class WolfAgent extends AbstractAgent {
 			 * correct direction of the acceleration.
 			 */
 
-			Vector acceleration = preyForce.multiply(10)
-					.add(mutualInteractionForce).add(forwardThrust)
+			preyForceFactor = hungry ? PREY_FORCE_WEIGHT : 0;
+			
+			Vector acceleration = preyForce.multiply(preyForceFactor)
+					.add(mutualInteractionForce)
+					.add(forwardThrust)
 					.add(arrayalForce);
 			double accelerationNorm = acceleration.getNorm();
 			if (accelerationNorm > maxAcceleration) {
@@ -122,7 +128,6 @@ public class WolfAgent extends AbstractAgent {
 		if (hungry)
 			return null;
 		else {
-			hungry = true;
 			List<IAgent> spawn = new ArrayList<IAgent>();
 			if (Math.random() < REPRODUCTION_RATE) {
 				Position pos;
@@ -144,13 +149,20 @@ public class WolfAgent extends AbstractAgent {
 		}
 	}
 
-	// This also decreases the deer's energy.
+	// This also decreases the wolfs energy.
 	@Override
 	public void updatePosition() {
 		super.updatePosition();
 		this.energy--;
-		if (energy == 0 || lifeLength > MAX_LIFE_LENGTH)
+		if (energy == 0 || lifeLength > MAX_LIFE_LENGTH) {
 			isAlive = false;
+		} else if (energy <= MAX_ENERGY * 0.6) {
+			hungry = true;
+		}
+		
+		if (sprintTimer-- == 0) {
+			isSprinting = true;
+		}
 	}
 
 	@Override
