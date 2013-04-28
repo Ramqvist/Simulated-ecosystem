8d3029faea61b0059853d26e2c9d19b3739107c1
Henrik Ernstsson <henern90@gmail.com>
2013-04-29 01:56:42 +0200

    Added the possibility for grass seeds

--- src/chalmers/dax021308/ecosystem/model/agent/GrassFieldAgent.java
+++ src/chalmers/dax021308/ecosystem/model/agent/GrassFieldAgent.java
@@ -1,6 +1,7 @@
 package chalmers.dax021308.ecosystem.model.agent;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 import chalmers.dax021308.ecosystem.model.environment.SurroundingsSettings;
@@ -19,12 +20,14 @@ public class GrassFieldAgent extends AbstractAgent {
 
 	private static final double REPRODUCTION_RATE = 0.1;
 	private static final double MAX_ENERGY = 200;
+	private List<GrassSeed> seeds;
 
 	public GrassFieldAgent(String name, Position pos, Color color, int width,
 			int height, Vector velocity, double maxSpeed, int capacity) {
 		super(name, pos, color, width, height, velocity, maxSpeed, 0, 0);
 		this.capacity = capacity;
 		energy = 50;
+		seeds = new ArrayList<>();
 	}
 
 	@Override
@@ -42,12 +45,21 @@ public class GrassFieldAgent extends AbstractAgent {
 	@Override
 	public List<IAgent> reproduce(IAgent agent, int populationSize,
 			SurroundingsSettings surroundings) {
+		for (int i = 0; i < seeds.size(); i++) {
+			if (seeds.get(i).isBlooming()) {
+				energy += seeds.get(i).getEnergy();
+				if (energy > MAX_ENERGY)
+					energy = MAX_ENERGY;
+				seeds.remove(i--);
+			}
+		}
 		if (Math.random() < REPRODUCTION_RATE) {
 			double energyProportion = (double) energy / (double) MAX_ENERGY;
 			double newEnergy = energy * energyProportion
 					* (1 - energyProportion) * 0.1;
 			// System.out.println(newEnergy);
 			energy += newEnergy;
+			// seeds.add(new GrassSeed(newEnergy));
 		}
 		int red = (int) (150.0 - 150.0 * (((double) energy) / MAX_ENERGY));
 		int green = (int) (55.0 + 200.0 * (((double) energy) / MAX_ENERGY));
@@ -80,8 +92,26 @@ public class GrassFieldAgent extends AbstractAgent {
 	public double impactForcesBy() {
 		// if it has a low amount of food it should negatively impact the agents
 		// who wants to eat it
-		// return 1.5 - energy / 100;
-		//but for now it just means less food = less impact
-		return energy/200;
+		 return 1 - energy / 100;
+		// but for now it just means less food = less impact
+		//return energy / 200;
+	}
+
+	private class GrassSeed {
+		private int lifeLength = 0;
+		private double energy;
+		private static final int TIME_TO_BLOOM = 100;
+
+		private GrassSeed(double energy) {
+			this.energy = energy;
+		}
+
+		private double getEnergy() {
+			return energy;
+		}
+
+		private boolean isBlooming() {
+			return lifeLength++ > TIME_TO_BLOOM;
+		}
 	}
 }
